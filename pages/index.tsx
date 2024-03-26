ac11108c4398312a92ed2b923c401c782fabde42
AkaCyberMac <akacybermac@aka.com>
2024-03-26 23:27:17 +0200

    fix an code update bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 
 import {AppChat} from '../src/apps/chat/AppChat';
-import {useRedirectToNewsOnUpdates} from '../src/apps/news/news.hooks';
 
 import {withLayout} from '~/common/layout/withLayout';
 
